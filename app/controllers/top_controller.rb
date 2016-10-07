f06bcc14936787e5970dba2283a41861cac91cd7
a1630ty <a1630ty@aiit.ac.jp>
2016-10-07 13:03:56 +0000

    add updatemoney

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,4 +4,7 @@ class TopController < ApplicationController
   end
   def setting
   end
+  def addMoney
+    current_user.total = params[:kane1]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 get '/'  => 'top#index'
+get '/addMoney:aMy'  => 'top#addMoney'
 get '/setting' => 'top#setting'
   # The priority is based upon order of creation: first created -> highest priority.
 #index'
