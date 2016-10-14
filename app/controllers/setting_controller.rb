3252f80ae5ee3d1e6e9c35aa7d52035347b9976b
a1630ty <a1630ty@aiit.ac.jp>
2016-10-14 15:11:21 +0000

    modgoal

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -1,9 +1,17 @@
 class SettingController < ApplicationController
   def reggoal
   end
+
   def regGoalMoney
-    goal1 = current_user.user_goals.build(buttonStr:params[:txtGoal].to_s,goalMoney:params[:money].to_i)
-    goal1.save
-    redirect_to action: :reggoal
+    if current_user.user_goals.exists? then
+      #goal1 = current_user.user_goals.find_by(user_id: current_user.id.to_i)
+      current_user.user_goals.delete_all()
+      goal1 = current_user.user_goals.build(buttonStr:params[:txtGoal].to_s,goalMoney:params[:money].to_i)
+      goal1.save
+    else
+      goal1 = current_user.user_goals.build(buttonStr:params[:txtGoal].to_s,goalMoney:params[:money].to_i)
+      goal1.save
+    end
+    redirect_to "/"
   end
 end
