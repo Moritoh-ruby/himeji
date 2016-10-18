c97964c9e73932205b838f5101346447f8712a6b
Tomomi Kawabe <girrrlande@gmail.com>
2016-10-18 15:51:56 +0000

    ビューでのツイート文言削除をコントローラに移動

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,39 +3,32 @@ class TopController < ApplicationController
   def index
   end
   def result
-     # Tweetに使う変数たち 
-     @hash_symbol = "%23"
-     @tweet_text_base = "円節約しました。"
-     @tweet_personal_temptation = "コーヒーを飲む"
-     @tweet_login_user_first_time = "これが初めての貯金です！"
-     @tweet_login_user_with_history_before = "いままでに"
-     @tweet_login_user_with_history_after = "円貯金しました！"
 
      @total_before = params[:kane1].to_i*100
-     # Tweetの基本文言作成
-　　 @tweet_text_common = "instead-of-coffee"
+     redirect_to "/" if @total_before == 0
 
-     #if user_signed_in? then
-     #  # DB更新
-     #  @total_after = @total_before + current_user.total.to_i
-     #  current_user.total = @total_after.to_s
-     #  current_user.save
-     # 
-     #  if @total_before == @total_after then
-     #    # はじめての貯金〜の文言を作成
-　　 # ��   @tweet_text = @tweet_text_common + @tweet_login_user_first_time
-     #  else
-     #    # いままでにXX円〜の文言を作成
-　　 # ��   @tweet_text = "#{@tweet_text_common}#{@tweet_login_user_with_history_before}#{@total_after}#{@tweet_login_user_with_history_after}"
-     #  end
-     #else
-       # 未ログインユーザ向けの文言を作成
-　　　 @tweet_text = "#{@tweet_text_common}"
-     #end
+     @tweet_personal_temptation = "コーヒーを飲む"
+     @tweet_text_base = "円節約しました。"
+     @tweet_login_user_first_time = "これが初めての貯金です！"
+     @tweet_login_user_with_history_before = "いままでに"
+     @tweet_login_user_with_history_after = "円節約しました！"
+
+     @tweet_text_common = "#{ @tweet_personal_temptation }代わりに#{@total_before}#{ @tweet_text_base }"
      
-     # Tweetの共通部分作成
+     if user_signed_in? then
+       @total_after = @total_before + current_user.total.to_i
+       current_user.total = @total_after.to_s
+       current_user.save
+       if @total_after == @total_before then
+         @tweet_text = "#{ @tweet_text_common }#{ @tweet_login_user_first_time }"
+       else
+         @tweet_text = "#{ @tweet_text_common }#{ @tweet_login_user_with_history_before }#{ @total_after }#{ @tweet_login_user_with_history_after }"
+       end
+     else
+       @tweet_text = @tweet_text_common
+     end
      @tweet_url = "https://goo.gl/D6RWc0"
-     @tweet_hashtags = "#{@hash_symbol}どや #{@hash_symbol}したつもり貯金 #{@hash_symbol}aiit_enpit"
+     @tweet_hashtags = "%23したつもり貯金 %23どや %23aiit_enpit"
   end
   
   def setting
