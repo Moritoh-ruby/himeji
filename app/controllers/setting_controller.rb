5b7eaebc8934c83c2ca6852d127bf12506b83cf6
a1630ty <a1630ty@aiit.ac.jp>
2016-10-21 10:02:02 +0000

    modresultandsetting

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -6,7 +6,8 @@ class SettingController < ApplicationController
   def regGoalMoney
     goal1 = current_user.user_goals.build(buttonStr:params[:txtGoal].to_s,goalMoney:params[:money].to_i,finish:false)
     goal1.save
-
+    current_user.total = 0
+    current_user.save
     redirect_to "/"
   end
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -9,10 +9,14 @@ class TopController < ApplicationController
      @total_after = @total_before + current_user.total.to_i
      current_user.total = @total_after.to_s
      current_user.save
-    if current_user.total.to_i > current_user.user_goals.find_by(finish: false).goalMoney then
-      rec = current_user.user_goals.find_by(finish: false)
-      rec.finish = true
-      rec.save
+    if current_user.user_goals.exists?(finish: false) 
+      if current_user.total.to_i >= current_user.user_goals.find_by(finish: false).goalMoney then
+        rec = current_user.user_goals.find_by(finish: false)
+        rec.finish = true
+        rec.save
+        current_user.total = 0
+        current_user.save
+      end
     end
   end
   end
