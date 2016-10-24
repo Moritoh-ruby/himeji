58be93cde31f76db3ef282fe0466b7fade8e6a09
Aritomo Moritoh <z14010am@aiit.ac.jp>
2016-10-24 13:14:20 +0000

    test

--- test/controllers/setting_controller_test.rb
+++ test/controllers/setting_controller_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 class SettingControllerTest < ActionController::TestCase
-  test "should get reggoal" do
-    get :reggoal
-    assert_response :success
-  end
-  test "should get regGoalMoney" do
-    get :regGoalMoney
-    assert_response :success
-  end
+  #test "should get reggoal" do
+   # get :reggoal
+    #assert_response :success
+ # end
+
 end
