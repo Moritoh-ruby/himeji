86cf93e1bff7644c891dc1f8d5a8ba0533deb4a2
Aritomo Moritoh <z14010am@aiit.ac.jp>
2016-10-24 10:21:44 +0000

    capybara

--- test/controllers/setting_controller_test.rb
+++ test/controllers/setting_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SettingControllerTest < ActionController::TestCase
   test "should get reggoal" do
     get :reggoal
-    assert_response : success
+    assert_response :success
   end
   test "should get regGoalMoney" do
     get :regGoalMoney
