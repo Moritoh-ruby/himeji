bcf9ea9680a0979a3889f77c25e36096ebd7d3bf
a1630ty <a1630ty@aiit.ac.jp>
2016-10-25 14:20:42 +0000

    addvalid

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -4,10 +4,10 @@ class SettingController < ApplicationController
   end
 
   def regGoalMoney
-    goal1 = current_user.user_goals.build(buttonStr:params[:txtGoal].to_s,goalMoney:params[:money].to_i,finish:false)
+    goal1 = current_user.user_goals.build(buttonStr:params[:txtGoal].to_s,goalMoney:params[:money].to_i,finish:false, total:0)
     goal1.save
     current_user.total = 0
     current_user.save
-    redirect_to "/"
+    redirect_to "/setting"
   end
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,35 +1,43 @@
 class TopController < ApplicationController
 
   def index
+     #サインインしていて、現在のユーザーのuser_idがUserGoalにあった場合の処理
+     if user_signed_in? && UserGoal.find_by(:user_id => current_user.id)
+     @usergoal = UserGoal.where(user_id:current_user.id).order("id desc").first
+     end
+     
   end
 
   def result
 
      @total_before = params[:kane1].to_i*100
-     @tweet_personal_temptation = "コーヒーを飲む"
+
+     @tweet_personal_temptation = "コーヒーを飲んだ"
      @tweet_text_base = "円節約しました。"
      @tweet_login_user_first_time = "これが初めての貯金です！"
      @tweet_login_user_with_history_before = "いままでに"
      @tweet_login_user_with_history_after = "円節約しました！"
 
      #共通で使うツイート文言　「XXXの代わりにYYY円節約しました！」
-     @tweet_text_common = "#{ @tweet_personal_temptation }代わりに#{@total_before}#{ @tweet_text_base }"
+     @tweet_text_common = "#{ @tweet_personal_temptation }つもりで#{@total_before}#{ @tweet_text_base }"
      
      if user_signed_in? then
        #DB更新処理
-       #TODO 保存先に目標テーブルを追加する
        @total_after = @total_before + current_user.total.to_i
        current_user.total = @total_after.to_s
        current_user.save
 
-       #DB更新処理
-       #TODO 目標テーブルの合計金額と比較して、目標金額を超えたばあいにfinishフラグをtrueにする
-       if current_user.user_goals.count > 0 && current_user.user_goals.find_by(finish: false) != nil then
-         if current_user.total.to_i >= current_user.user_goals.find_by(finish: false).goalMoney then
-           rec = current_user.user_goals.find_by(finish: false)
-           rec.finish = true
-           rec.save
+       #目標テーブルがあれば最新の目標に追加（この時点でfinishフラグは考えない）
+       if current_user.user_goals.count > 0 then
+           rec = current_user.user_goals.last
+           rec.total = rec.total + @total_before
+
+       #目標テーブルが金額を超えていたらfinish=trueにする
+         if rec.finish == false && rec.total >= rec.goalMoney then
+              rec.finish = true
+
          end
+         rec.save
        end
 
        if @total_after == @total_before then
