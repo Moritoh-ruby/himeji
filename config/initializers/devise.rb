a3052c1e1a3741fdac1c1307eb8252c56726c132
a-rom <a1623at@aiit.ac.jp>
2016-10-26 02:23:15 +0000

    twitterのOAuth認証実装完了しました

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  config.omniauth :twitter, 'mlkkuQPLQGRLksTgIXi10FiSp', '4q9XdeYvNf3xc0GYJp5EcBkR4ngSF3ooQUX58Eiz7AFMwzCIPX'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
