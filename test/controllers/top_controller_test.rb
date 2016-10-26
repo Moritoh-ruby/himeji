9d85fd87f786a46dabc9f12d617181768fac677f
Tomomi Kawabe <girrrlande@gmail.com>
2016-10-26 14:25:53 +0000

    ツイート文言に目標と目標金額までの残額を含める

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -9,7 +9,7 @@ class TopControllerTest < ActionController::TestCase
   end
 
   test 'unauthenticated users tweet text' do
-    tweet_text = "コーヒーを飲んだつもりで100円節約しました。"
+    tweet_text = "コーヒーを飲むかわりに、100円を節約します。"
     get :result, :kane1 => 1
     assert_equal tweet_text, assigns[:tweet_text]
   end
@@ -31,6 +31,41 @@ class TopControllerTest < ActionController::TestCase
     hashtags = "%23したつもり貯金 %23どや %23aiit_enpit"
     get :result
     assert_equal hashtags, assigns[:tweet_hashtags]
+  end
+
+  test 'tweet w/o goal and first time' do
+    sign_in users(:chris)
+    tweet_text = "コーヒーを飲むかわりに、100円を節約します。これが初めての貯金です！"
+    get :result, :kane1 => 1
+    assert_equal tweet_text, assigns[:tweet_text]
+  end
+
+  test 'tweet w/o goal but not first time' do
+    sign_in users(:dean)
+    tweet_text = "コーヒーを飲むかわりに、100円を節約します。いままでに1100円貯金しました！"
+    get :result, :kane1 => 1
+    assert_equal tweet_text, assigns[:tweet_text]
+  end
+
+  test 'tweet w/ unfinished goal will be the goal itself and remaining amount' do
+    sign_in users(:alice)
+    tweet_text = "コーヒーを飲むかわりに、100円を沖縄旅行に使います。目標金額まであと400円です！"
+    get :result, :kane1 => 1
+    assert_equal tweet_text, assigns[:tweet_text]
+  end
+
+  test 'tweet w/ finished goal only will be the goal and finished statement' do
+    sign_in users(:bob)
+    tweet_text = "コーヒーを飲むかわりに、100円をニートになるために使います。目標の1000円を達成しました！"
+    get :result, :kane1 => 1
+    assert_equal tweet_text, assigns[:tweet_text]
+  end
+
+  test 'tweet w/ goal just finished will be the goal and finished statement' do
+    sign_in users(:alice)
+    tweet_text = "コーヒーを飲むかわりに、600円を沖縄旅行に使います。目標の1000円を達成しました！"
+    get :result, :kane1 => 6
+    assert_equal tweet_text, assigns[:tweet_text]
   end
 
 end
