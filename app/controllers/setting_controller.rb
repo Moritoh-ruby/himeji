d50573f5d0ff7dbd6a9f78d166e4872e790779d5
a1630ty <a1630ty@aiit.ac.jp>
2016-10-26 14:55:47 +0000

    aras2

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -6,8 +6,6 @@ class SettingController < ApplicationController
   def regGoalMoney
     goal1 = current_user.user_goals.build(buttonStr:params[:txtGoal].to_s,goalMoney:params[:money].to_i,finish:false, total:0)
     goal1.save
-    current_user.total = 0
-    current_user.save
     redirect_to "/setting"
   end
 end
