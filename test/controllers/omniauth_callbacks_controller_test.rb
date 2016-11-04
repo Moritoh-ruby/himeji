526c1863dd31adf27b31190d548396addf13800a
a-rom <a1623at@aiit.ac.jp>
2016-11-04 15:14:18 +0000

    一旦omniauth_callbacks_controller_test.rbのテストコードを削除しました

--- test/controllers/omniauth_callbacks_controller_test.rb
+++ test/controllers/omniauth_callbacks_controller_test.rb
@@ -1,12 +1,5 @@
 require 'test_helper'
 
 class OmniauthCallbacksControllerTest < ActionController::TestCase
-def self.from_omniauth(auth)
-    where(provider: auth.provider, id: auth.uid).first_or_create do |user|
-     user.provider = auth.provider
-     user.name = auth.info.nickname
-    end
-end
-
 
 end
