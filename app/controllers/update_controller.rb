2577a86bb29b6b7cfc6a7b6819ddc7df2db61843
Moritoh-ruby <moritoh@lime.plala.or.jp>
2016-11-06 08:28:17 +0000

    edit

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -8,12 +8,14 @@ def update
   @user_goal = UserGoal.find_by(user_id: current_user.id,finish: false)
   @user_goal.buttonStr = params[:buttonStr].to_s 
   @user_goal.goalMoney = params[:goalmoney].to_i
-  @user_goal.save
-    redirect_to '/setting'
-
   
+if @user_goal.goalMoney > params[:total].to_i then
+   @user_goal.save
+   else
+ redirect_to '/setting'
+ end
+   
 end
-
 #private
 
 #  def user_goal_params
