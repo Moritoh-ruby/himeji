aa97be4562a142620e5db37c222fa574b68f1ee2
Moritoh-ruby <moritoh@lime.plala.or.jp>
2016-11-10 11:36:40 +0000

    edit

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -9,7 +9,7 @@ def update
   @user_goal.buttonStr = params[:user_goal][:buttonStr].to_s
   @user_goal.goalMoney = (params[:goalMoney].to_i) * 1000
   
-if  @user_goal.goalMoney > params[:total].to_i/1000 then 
+if  @user_goal.goalMoney > @user_goal.total then 
     @user_goal.save
     redirect_to '/setting'     
   else    
