e21c1a50fcdedfc0a918d14c05ba46f0705143f8
Moritoh-ruby <moritoh@lime.plala.or.jp>
2016-11-15 09:53:50 +0000

    wakaranai

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,13 @@ ActiveRecord::Schema.define(version: 20161103083254) do
     t.datetime "updated_at"
   end
 
-
+   create_table "excuses", force: :cascade do |t|
+     t.string   "excuse"  
+     t.integer  "endurance_id"
+     t.integer  "waste_money"
+     t.datetime "created_at",  null: false
+     t.datetime "updated_at",  mull: false
+  end 
 
   create_table "user_goals", force: :cascade do |t|
     t.integer  "user_id",    limit: 8
@@ -47,12 +53,12 @@ ActiveRecord::Schema.define(version: 20161103083254) do
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "updated_at",                           null: false
     t.string   "total"
     t.string   "provider"
     t.string   "name"
   end
-
+  
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
