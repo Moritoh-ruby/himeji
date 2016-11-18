76662e5954c05d45971d87b39dc4be04dbedc113
Moritoh-ruby <moritoh@lime.plala.or.jp>
2016-11-18 13:27:20 +0000

    fade out

--- app/controllers/excuse_controller.rb
+++ app/controllers/excuse_controller.rb
@@ -4,11 +4,12 @@ class ExcuseController < ApplicationController
  def create_excuse
    
  @current_record = Endurance.where(user_id:current_user.id).order("id desc").first
- @excuse = @current_record.excuses.create(excuse:params[:endurance_id])
- redirect_to '/show'        
+ excuse = @current_record.excuses.create(excuse:params[:excuse].to_s)
+ redirect_to '/show'       
+
  
    def show
-   @excuse= Excuse.find(params[:excuse , :waste_money])  
+    
    end 
  end
 
