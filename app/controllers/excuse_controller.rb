7e35ca4ea045def34485b525f68cf8b97a92effd
Moritoh-ruby <moritoh@lime.plala.or.jp>
2016-11-19 01:20:26 +0000

    wakaranai

--- app/controllers/excuse_controller.rb
+++ app/controllers/excuse_controller.rb
@@ -1,19 +1,21 @@
 class ExcuseController < ApplicationController
  
-  
- def create_excuse
-   
- @current_record = Endurance.where(user_id:current_user.id).order("id desc").first
- excuse = @current_record.excuses.create(excuse:params[:excuse].to_s)
- redirect_to '/show'       
-
- 
-   def show
-    
-   end 
+ def list
+ @excuses = Excuse.all 
  end
-
-
+ 
+  def create_excuse
+ 
+ @current_record = Endurance.where(user_id:current_user.id).order("id desc").first
+ excuse = @current_record.excuses.create(excuse: params[:excuse].to_s)
+ 
+ redirect_to '/show'       
+ 
+  def show
+     
+  end 
+ end
 end
+
 
 
