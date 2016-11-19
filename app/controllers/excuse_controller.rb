23917bb5f97546ce50aba44e9e27dd456e18f5ba
Moritoh-ruby <moritoh@lime.plala.or.jp>
2016-11-19 23:16:06 +0000

    money1?

--- app/controllers/excuse_controller.rb
+++ app/controllers/excuse_controller.rb
@@ -4,8 +4,9 @@ class ExcuseController < ApplicationController
   def create_excuse
     current_record = Endurance.where(user_id:current_user.id).order("id desc").first
     current_record.excuses.create(excuse: params[:my_form].to_s)
+    current_record.excuses.create(waste_money: params[:money1].to_i * 1000)
     @cast = params[:my_form].to_s
-
+    @base = params[:money1] .to_i * 1000 
 #    @current_excuse = Excuse.where
 #    @current_excuse= @current_record.excuse 
 #    redirect_to '/show'       
