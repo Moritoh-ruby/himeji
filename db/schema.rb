fc6c840586a50e2a3a0c5b81b31ff141b1fb869d
Tomomi Kawabe <girrrlande@gmail.com>
2016-11-20 11:06:08 +0000

    index.html.erbの処理をtop_controllerへ移動

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103083254) do
+ActiveRecord::Schema.define(version: 20161112064115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,13 +23,13 @@ ActiveRecord::Schema.define(version: 20161103083254) do
     t.datetime "updated_at"
   end
 
-   create_table "excuses", force: :cascade do |t|
-     t.string   "excuse"  
-     t.integer  "endurance_id"
-     t.integer  "waste_money"
-     t.datetime "created_at",  null: false
-     t.datetime "updated_at",  mull: false
-  end 
+  create_table "excuses", force: :cascade do |t|
+    t.string   "excuse"
+    t.integer  "endurance_id"
+    t.integer  "waste_money"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "user_goals", force: :cascade do |t|
     t.integer  "user_id",    limit: 8
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20161103083254) do
     t.string   "provider"
     t.string   "name"
   end
- 
+
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
