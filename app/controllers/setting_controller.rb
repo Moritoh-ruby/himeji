20fc0306490f6dc22ca9a1bbed0c11350be43526
Tomomi Kawabe <girrrlande@gmail.com>
2016-11-24 13:35:36 +0000

    スマホだとテーブルが100%表示になるように

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -2,6 +2,7 @@ class SettingController < ApplicationController
   def rgtr_goal
     if user_signed_in? 
       @goals = current_user.user_goals.all.order("finish asc")
+      @current_goal = current_user.user_goals.order("id desc").first
       if current_user.endurances.exists?
         @current_record = Endurance.where(user_id:current_user.id).order("id desc").first
         @current_endurance = @current_record.endurance
